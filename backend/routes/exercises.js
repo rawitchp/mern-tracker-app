94f65a559745d60ad3210d36d676b60a18e0765b
Rawitch Payakkawan <rawitch13@gmail.com>
2023-01-16 04:17:35 +0700

    finish

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,44 +1,42 @@
-const router = require('express').Router();
+const router = require('express').Router({ mergeParams: true });
 let Exercise = require('../models/exercise.model');
 
-router.route('/').get((req, res) => {
+router.get('/', async (req, res) => {
   Exercise.find()
     .then((exercises) => res.json(exercises))
     .catch((err) => res.json(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+router.post('/add', async (req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
   const newExercise = new Exercise({ username, description, duration, date });
-  newExercise
+  await newExercise
     .save()
     .then(() => res.json('Exercise added!'))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
-
-router.route('/:id').get((req, res) => {
-  Exercise.findById(req.params.id)
+router.get('/:id', async (req, res) => {
+  await Exercise.findById(req.params.id)
     .then((exercise) => res.json(exercise))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').delete((req, res) => {
+router.delete('/:id', async (req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
     .then((exercise) => res.json('Exercise deleted.'))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
-router.route('/update/:id').post((req, res) => {
+router.put('/update/:id', async (req, res) => {
   Exercise.findById(req.params.id)
     .then((exercise) => {
       exercise.username = req.body.username;
       exercise.description = req.body.description;
       exercise.duration = Number(req.body.duration);
       exercise.date = Date.parse(req.body.date);
-
       exercise
         .save()
         .then(() => res.json('Exercise updated!'))
